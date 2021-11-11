14873af7706d197a932a542daadc78b99a89ca12
BENBRIKMouad <61920384+BENBRIKMouad@users.noreply.github.com>
2021-11-11 21:23:24 +0100

    Ajout EJB CustomerManager

---
+++ src/main/java/ma/benbrik/customerappbenbrikmouad/session/CustomerManager.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ma.benbrik.customerappbenbrikmouad.session;
+
+import java.util.List;
+import javax.annotation.Resource;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import ma.benbrik.customerappbenbrikmouad.entities.Customer;
+
+/**
+ *
+ * @author benbrik_mouad
+ * <p>
+ * For this specific implementation of the interface, you're better off
+ * calling the more efficient {@link foo} method, provided you have the
+ * additional arguments!
+ * </p>
+ */
+@Stateless
+public class CustomerManager {
+
+    @PersistenceContext(unitName = "customerPresistUnit")
+    private EntityManager em;
+    @Resource
+    private javax.transaction.UserTransaction utx;
+    
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    
+    public List<Customer> getAllCustomers() { 
+       Query query = em.createNamedQuery("Customer.findAll");
+       return query.getResultList(); 
+    }  
+        
+    public Customer update(Customer customer) {
+      return em.merge(customer);
+    }  
+
+    public void persist(Object object) {
+            em.persist(object);
+    }
+}
